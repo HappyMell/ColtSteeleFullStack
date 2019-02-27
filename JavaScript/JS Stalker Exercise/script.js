e09ff3adfac556ca847bfb702f03546b0d8480fe
HappyMell <mellisamadrid@hotmail.com>
2019-02-27 16:34:30 +0000

    Bootstrap finished, Javascript intro

---
+++ JavaScript/JS Stalker Exercise/script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+var daysAlive = age * 365;
+
+
+console.log(`So your name is ${firstName} ${lastName} and your age is ${age} ?
+                You've been alive for ${daysAlive} days.`);
