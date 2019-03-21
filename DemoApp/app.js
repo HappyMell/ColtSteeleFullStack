19b458c06555b113fba9f55e45badb31987061e5
HappyMell <mellisamadrid@hotmail.com>
2019-03-21 14:41:12 +0000

    remove

--- DemoApp/app.js
+++
@@ -1,5 +0,0 @@
-var kitty = require("cat-me");
-var jokes = require("knock-knock-jokes");
-
-//console.log(kitty());
-console.log(jokes());
--- Myshop/listProducts.js
+++
@@ -1,9 +0,0 @@
-var faker = require("faker");
-
-function product() {
-  for(var i = 0; i <= 10; i++) {
-      console.log(faker.fake("{{commerce.productName}} - ${{commerce.price}}"));
-  }
-}
-
-console.log(product())
